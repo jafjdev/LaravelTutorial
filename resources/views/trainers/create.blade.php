dbb9bf1fc81288adf54263d85309f68bfce13b4f
Unknown <jafjdev@gmail.com>
2018-06-21 13:02:29 -0430

    Blade Trainer
    Aplicado Blade en Trainers-Create

---
+++ resources/views/trainers/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title','Trainers Create')
+
+<body>
+
+	<div class="container">
+		<div class="form-group">
+			<label for="">Nombre</label>
+			<input type="text" name="" class="form-control">
+			<button type="submit" class="btn btn-primary">Guardar</button>
+		</div>	
+	</div>
+</body>
+
+</html>
